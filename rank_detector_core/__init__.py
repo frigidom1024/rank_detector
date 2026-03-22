53ac76ce955ee566628a46bbc27548a95279a676
LinRongQiang <3500954350@qq.com>
2026-03-22 10:47:55 +0800

    feat: 导出保存标志常量

--- rank_detector_core/__init__.py
+++ rank_detector_core/__init__.py
@@ -1,3 +1,19 @@
-from .legend_recognizer import AIAwareLegendRecognizer, RecognitionResult
+from .legend_recognizer import (
+    AIAwareLegendRecognizer,
+    RecognitionResult,
+)
 
-__all__ = ["AIAwareLegendRecognizer", "RecognitionResult"]
+# 保存标志常量也可以直接导入
+SAVE_NONE = AIAwareLegendRecognizer.SAVE_NONE
+SAVE_UNKNOWN = AIAwareLegendRecognizer.SAVE_UNKNOWN
+SAVE_LEGENDARY = AIAwareLegendRecognizer.SAVE_LEGENDARY
+SAVE_ALL = AIAwareLegendRecognizer.SAVE_ALL
+
+__all__ = [
+    "AIAwareLegendRecognizer",
+    "RecognitionResult",
+    "SAVE_NONE",
+    "SAVE_UNKNOWN",
+    "SAVE_LEGENDARY",
+    "SAVE_ALL",
+]
